2ede7bd03112c856bbe1a6784b32b443041b790d
del-ui <wambuivictor@students.must.ac.ke>
2023-03-02 08:23:22 +0300

    commit

--- components/ApiData.js
+++ components/ApiData.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export  {fetchData}() {
+export  {fetchData}; {
   return axios.get('https://api.example.com/data')
     .then(response => response.data)
     .catch(error => console.error(error));
